1beb030aee44bb5e9343db7243515851f8741f04
lz-junior <correajr.dev@gmail.com>
2024-09-20 23:35:20 -0300

    server.ts updated

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -1,21 +1,26 @@
 import fastify from 'fastify'
 import { createGoal } from '../functions/create-goal'
-import { serializerCompiler, validatorCompiler, ZodTypeProvider } from 'fastify-type-provider-zod'
+import { serializerCompiler, validatorCompiler, type ZodTypeProvider } from 'fastify-type-provider-zod'
 import z from 'zod'
 
-const app = fastify()
+const app = fastify().withTypeProvider<ZodTypeProvider>()
 
-app.post('/goals', async request => {
-  const createGoalSchema = z.object({
-    title: z.string(),
-    desiredWeeklyFrequency: z.number().int().min(1).max(7),
-  })
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
 
-  const body = createGoalSchema.parse(request.body)
+app.post('/goals', {
+  schema: {
+    body: z.object({
+      title: z.string(),
+      desiredWeeklyFrequency: z.number().int().min(1).max(7),
+    }),
+  }
+}, async request => {
+  const { title, desiredWeeklyFrequency } = request.body
 
   await createGoal({
-    title: body.title,
-    desiredWeeklyFrequency: body.desiredWeeklyFrequency,
+    title,
+    desiredWeeklyFrequency,
   })
 })
 
